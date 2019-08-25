88c86673c14f6b14d56f95666e269df1eb2d0aa2
Victor Villca <victor.villca.v@gmail.com>
2019-08-25 23:08:45 +0100

    LV-0001: Creating Task Component Vue and implementing controller Use Case Notes

---
+++ database/factories/NotaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Nota;
+use Faker\Generator as Faker;
+
+$factory->define(Nota::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">App de Notes</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -15,8 +16,11 @@
                     @endif
 
                     You are logged in!
-                    <example-component/>
+                    <task-component/>
+
+                    
                 </div>
+                
             </div>
         </div>
     </div>
